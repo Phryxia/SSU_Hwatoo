4a26899845f08f6520b9664dbd7e9f6290d8f7e3
jmk1118@naver.com <rlals01>
2014-11-11 01:14:04 +0900

    main

---
+++ projectmain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+int a;
+	printf("1.game start\n2.load game\n3.exit\n");
+scanf("%d", &a);
+if(a==1)
+printf("game start");
+elseif(a==2)
+printf("laod");
+elseif(a==3)
+return 0;
+else
+printf("잘못된 입력입니다.")
+}
